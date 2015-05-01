c44f45bc7319e51735e3c8973d0a906657ccd49c
Brendan O'Neil <brendanroneil@gmail.com>
2015-05-01 13:45:31 -0400

    BEEP BOOP

---
+++ main.py
@@ -0,0 +1,16 @@
+__author__ = 'brendan'
+
+import pandas as pd
+import numpy as np
+from datetime import datetime as dt
+from matplotlib import pyplot as plt
+
+
+class Backtest():
+
+    def __init__(self):
+        self.prices = #get prices
+
+    def strategy_SSA(self):
+        
+    def generate_pnl(self, exposure):
