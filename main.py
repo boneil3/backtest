8b5600f0dcff76648b3b78367842ad83d27bdc38
Brendan O'Neil <brendanroneil@gmail.com>
2015-05-06 01:23:36 -0400

    beep boop

--- main.py
+++ main.py
@@ -12,5 +12,13 @@ class Backtest():
         self.prices = #get prices
 
     def strategy_SSA(self):
-        
+        """ Generate exposure from prices """
+
     def generate_pnl(self, exposure):
+        delta_price = self.prices.pct_change()
+        delta_portfolio = delta_price * exposure
+
+        pnl = pd.DataFrame(np.ones(delta_portfolio.shape[0]), index=self.prices.index)
+        for i in range(len(pnl.index) - 1):
+            pnl.items[i+1] = (pnl.items[i] + 1) * delta_portfolio.items[i]
+            
